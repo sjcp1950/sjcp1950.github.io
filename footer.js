137b4f361bc7cac5ec66575da91fc7f7791bf9dc
sjcp1950 <89735823+sjcp1950@users.noreply.github.com>
2021-08-29 23:25:22 +0900

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$("body").append(	"<footer>"+
+					"<span>"+
+						"<span>TEL. : (053) 355-1950</span>"+
+						"<span>FAX. : (053) 355-1955</span>"+
+						"<span>E-mail : <a href='mailto:sjcp1950@naver.com'>sjcp1950@naver.com</a></span>"+
+						"<span>41595 <a title='세진냉동 - 카카오맵' href='https://map.kakao.com/?map_type=TYPE_MAP&from=roughmap&srcid=1618163644&itemId=1618163644&q=%EC%84%B8%EC%A7%84%EB%83%89%EB%8F%99&urlX=857547.0&urlY=664686.0'>대구 북구 칠성남로 29-2</a></span>"+
+					"</span>"+
+					"<span>"+
+						"<span>© 2021. 세진냉동<span>"+
+					"</span>"+
+				"</footer>");
+});
---
+++ header.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+	$("body").prepend(	"<a href='/' class='header'><header>세진냉동</header></a>");
+});
