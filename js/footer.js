744e6c0b33a520acfad23a28b8d2b5e825c3ab44
sjcp1950 <89735823+sjcp1950@users.noreply.github.com>
2021-08-30 02:24:39 +0900

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$("body").append(	"<footer>"+
 					"<span>"+
-						"<span>TEL : <a href="tel:053-355-1950">(053) 355-1950</a></span>"+
+						"<span>TEL : <a href='tel:053-355-1950'>(053) 355-1950</a></span>"+
 						"<span>FAX : (053) 355-1955</span>"+
 						"<span>E-mail : <a href='mailto:sjcp1950@naver.com'>sjcp1950@naver.com</a></span>"+
 						"<span>41595 <a title='세진냉동 - 카카오맵' href='https://map.kakao.com/?map_type=TYPE_MAP&from=roughmap&srcid=1618163644&itemId=1618163644&q=%EC%84%B8%EC%A7%84%EB%83%89%EB%8F%99&urlX=857547.0&urlY=664686.0'>대구 북구 칠성남로 29-2</a></span>"+
