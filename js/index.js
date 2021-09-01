80c19f32f8b70fbce3e3c7b58c9ca0678f50580a
sjcp1950 <sjcp1950@naver.com>
2021-09-01 16:55:00 +0900

    .

--- js/index.js
+++ js/index.js
@@ -1,5 +1,51 @@
 $(document).ready(function() {
 	$("img").click(function() {
-		location.href = $(this).attr("src");
+		if ( $(this).parent().attr("class") != "sliding" ) {
+			location.href = $(this).attr("src");
+		}
+	});
+
+	$(".slide").each(function(slide_i, slide) {
+		var slide_cnt = $(slide).children().length;
+
+		$(slide).children().attr("class", "sliding_in");
+		$(slide).children().wrap("<div class='sliding'></div>");
+		$(slide).children().wrapAll("<div class='slide_list'></div>");
+		$(slide).find("div").css("width", $(slide).css("width"));
+		$(slide).find("div").css("height", $(slide).css("height"));
+		$(slide).find(".sliding").children().css("max-width", $(slide).css("width"));
+		$(slide).find(".sliding").children().css("max-height", $(slide).css("height"));
+		$(slide).append("<div class='slide_btn'></div>");
+
+		var offset = $(slide).width();
+		for (var i=1; i<slide_cnt+1; i++) {
+			var sliding = $(slide).find(".sliding:nth-child(" + i + ")");
+			$(sliding).css("right", offset+10 - offset*i - 10*i);
+			// $(sliding).css("padding-top", ($(sliding).height() - $(sliding).children().height())/2);
+
+			$(slide).children("div.slide_btn").append("<span class='slide_btn'></span>");
+		}
+		$(slide).find("span.slide_btn").css("margin-left", ($("div.slide_btn").width() - $("span.slide_btn").width() * slide_cnt) / (slide_cnt+1));
+
+		$(slide).find("span.slide_btn").click(function() {
+			clearTimeout(slice_loop);
+			var nth = $(this).index() + 1;
+			$(this).parent().children().css("background-color", "");
+			$(this).css("background-color", "var(--border-color)");
+			$(slide).find(".slide_list").css("margin-left", offset+10 - offset*nth - 10*nth);
+
+			if ( $(this).next().length == 1 ) {
+				var slide_click = $(this).next();
+			} else {
+				var slide_click = $(this).parent().children("span.slide_btn:first-child");
+			}
+			var slice_loop = setTimeout(function() {
+				$(slide_click).trigger("click");
+			}, 3500);
+			$(this).parent().children().click(function() {
+				clearTimeout(slice_loop);
+			});
+		});
+		$(slide).find("span.slide_btn:first-child").trigger("click");
 	});
 });
--- js/template.js
+++ js/template.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
 	$("section").before(
 		"<nav>"+
 			"<ul>"+
-				"<li><a href='/'>nav_1</a></li>"+
-				"<li><a href='/map/'>지도</a></li>"+
+				"<li><a href='/'>회사소개</a></li>"+
+				"<li><a href='/map/'>오시는 길</a></li>"+
 			"</ul>"+
 		"</nav>");
 	var path_check = pathname.split("/");
@@ -23,13 +23,12 @@ $(document).ready(function() {
 	$("section").after(
 		"<footer>"+
 			"<span>"+
-				"<span>TEL : <a href='tel:053-355-1950'>(053) 355-1950</a></span>"+
+				"<span>H.P : <a href='tel:01045451998'>010-4545-1998</a></span>"+
+				"<span>TEL : <a href='tel:0533551950'>(053) 355-1950</a></span>"+
 				"<span>FAX : (053) 355-1955</span>"+
 				"<span>E-mail : <a href='mailto:sjcp1950@naver.com'>sjcp1950@naver.com</a></span>"+
 				"<span>41595 <a title='세진냉동 - 카카오맵' href='https://map.kakao.com/?map_type=TYPE_MAP&from=roughmap&srcid=1618163644&itemId=1618163644&q=%EC%84%B8%EC%A7%84%EB%83%89%EB%8F%99&urlX=857547.0&urlY=664686.0'>대구광역시 북구 칠성남로 29-2</a></span>"+
-			"</span>"+
-			"<span>"+
-				"<span>© 2021. 세진냉동<span>"+
+				"<span>© 2021. 세진냉동 All Rights Reversed<span>"+
 			"</span>"+
 		"</footer>");
 });
